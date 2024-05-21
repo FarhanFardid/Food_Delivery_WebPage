c3c143d591b3ede5ad802fb6343f3458a4eff80c
FarhanFardid <farhanfardid009@gmail.com>
2024-05-21 16:30:17 +0600

    Menu section started

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
+import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Header></Header>
+      <Menu></Menu>
 
     
     </>
---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+
+
+const Menu = () => {
+    return (
+        <div className="mt-[150px] text-center container-fluid">
+            <div className="w-[610px]  mx-auto py-8">
+                <h2 className="text-4xl py-3 font-bold px-20" >More Than <span className="text-[#FF5331]">10,000 </span> Dishes To Order!</h2>
+                <p className="px-16 text-sm">Welcome to The Biggest Network of Food Ordering & Delivery</p>
+            </div>
+
+        </div>
+    );
+};
+
+export default Menu;
