339af3d8eb58316f12fdbc9d07e1ba46c98bcdcc
FarhanFardid <farhanfardid009@gmail.com>
2024-05-21 20:52:57 +0600

    Order section completed with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 import Navbar from "./components/Navbar";
+import Order from "./components/Order";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar></Navbar>
       <Header></Header>
       <Menu></Menu>
+      <Order></Order>
 
     
     </>
