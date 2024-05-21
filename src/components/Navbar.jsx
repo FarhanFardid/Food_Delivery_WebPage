4801bdd81376d18fc261c2788115f55bf9393989
FarhanFardid <farhanfardid009@gmail.com>
2024-05-21 23:49:12 +0600

    Everything is ok... Ready for production deploy..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,6 @@ const Navbar = () => {
             type="button"
             className="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-full text-xs md:text-sm px-3 md:px-5  py-1.5 md:py-2.5 me-1 md:me-2 mb-1 md:mb-2 btn-xs md:btn-md "
           >
-            
             Login
           </button>
           <button
