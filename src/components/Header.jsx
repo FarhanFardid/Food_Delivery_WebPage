d1bbc835b92c16c5a30e20b763f89805370d5880
FarhanFardid <farhanfardid009@gmail.com>
2024-05-21 15:54:24 +0600

    Responsiveness for mobile device Header Section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,12 +5,12 @@ import courierImg from '../assets/images/courier.png'
 
 const Header = () => {
     return (
-        <div className="hero h-[478px] bg- mt-10 p-16 top-[221px] ">
+        <div className="container hero h-[478px] p-5 md:p-16 top-[20px] md:top-[221px] ">
         <div className="hero-content flex flex-col md:flex-row-reverse items-center justify-center">
           <img src={courierImg} className="w-[570px] h-[478px] rounded-xl shadow-2xl flex-1 transform scale-x-[-1] bg-[#F2EBDA] pt-5" />
           <div className="flex-1">
-            <h1 className="text-5xl font-bold w-[585px] h-[130px]">Your Favorite Food Delivery Partner</h1>
-            <p className="w-[528px] h-[60px] font-normal">The food at your doorstep. Why starve when you have us. You hunger partner. Straight out of the oven to your doorstep.</p>
+            <h1 className="text-2xl md:text-5xl mt-3 md:mt-0 ms-6 md:ms-0 font-bold w-auto h-[40px] md:w-[585px] md:h-[130px] ">Your Favorite Food Delivery Partner</h1>
+            <p className="text-sm  md:text-lg md:w-[528px] md:h-auto font-normal pb-2 md:pb-4">The food at your doorstep. Why starve when you have us. Your hunger partner. Straight out of the oven to your doorstep.</p>
           
     <div className="flex md: flex-row w-[464px] h-[64px]">
       <input type="text" id="search-navbar" className="block w-[330px] h-[52px] p-2  text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:border-blue-500 me-2" placeholder=" Enter Your Delivery Location" />
