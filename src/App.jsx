b0dd55c863d15c0533cfd6792a0e040574459396
FarhanFardid <farhanfardid009@gmail.com>
2024-05-21 11:44:18 +0600

    Tailwind CSS installed and  included in the project environmet

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Crave Courier - Food Delivery</h1>
+      <h1 className="text-4xl text-red-800">Crave Courier - Food Delivery</h1>
     </>
   );
 }
